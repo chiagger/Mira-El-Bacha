76d3000b925ab670fad46c003ab8ceef6b31fe9b
chiagger <busca.chiara.cb@gmail.com>
2023-12-19 13:49:29 +0100

    changed CV and fixed styling

--- Presenter/CV.js
+++ Presenter/CV.js
@@ -24,14 +24,16 @@ function appendWorkExperience(jobTitle, employer, date, description) {
     workExperienceDiv.appendChild(workexperience1);
 }
 
-appendWorkExperience("Event Photographer", "", "2017 - Present", "Photographer for Cassero LGBT center, JamSession music school, film festivals and private events.");
+
+appendWorkExperience("1st Assistant Director", "Datura Daydream | ","2022", "Short film production.");
 appendWorkExperience("1st Assistant Director", "The Morning After | ","2022", "Short film production with Roundhouse.");
-appendWorkExperience("1st Assistant Director", "Datura Daydream | ","2022", "Production of a series of interviews on reading with important people for Bologna.");
-appendWorkExperience("Short Film Selection for Film Festival", "Gli anni in tasca | ","2017 - 2020", "Selection of short films to be shown in competition at YoungAbout Film Festival.");
-appendWorkExperience("Film Judge", "Gli anni in tasca | ","2015 - 2020", "Film judge in the youth jury at YoungAbout Film Festival.");
+appendWorkExperience("1st Assistant Director", "Rise Like Water | ","2021", "Short film production.");
+appendWorkExperience("1st Assistant Director", "Fragile | ","2021", "Short film production.");
+appendWorkExperience("Production Assistant", "This Person Does Not Exist | ","2022", "Short film production.");
 appendWorkExperience("Documentary Film Maker", "Il sole in cantina | ","2019", "Production of a short documentary about music and history. Production of a series of interviews.");
-appendWorkExperience("Film Festival Organiser", "Some Prefer Cake and Genderbender | ","2017 - 2019", "Usher, Photography, Technical Organisation of the Festival, Sales Operator.");
+appendWorkExperience("Film Festival Organiser", "Some Prefer Cake and Genderbender | ","2014 - 2020", "Administrative work to put the festival together, from getting more volunteers to helping choose the films to be screened.");
 appendWorkExperience("Film Tutor", "Mediateca di San Lazzaro | ","2019", "Tutor younger people in making a short video.");
+appendWorkExperience("Events Organiser", "Cassero LGBT Center | ","2015 - 2020", "Coordinated a team to organise events and awareness campaigns. Did a lot of team-building.");
 appendWorkExperience("Internship TV", "Twofour54 | ","2018", "Internship in television where I rehearsed every role in a live news programme.");
 appendWorkExperience("Documentary Film Maker", "Younet | ","2017 - 2018", "Cultural exchange in Germany to make a documentary about young jugglers. Interviewer.");
 appendWorkExperience("Director", "Salaborsa, Cineteca di Bologna | ","2014 - 2015", "Production of a short film for the municipal library.");
