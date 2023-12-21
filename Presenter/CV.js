5c477eed7e40a691590ebe3fc47a272b09efeaf3
chiagger <busca.chiara.cb@gmail.com>
2023-12-21 21:31:11 +0100

    changed CV order and added horizontal separator

--- Presenter/CV.js
+++ Presenter/CV.js
@@ -1,11 +1,12 @@
 const column1 = document.querySelector(".column1");
 const column2 = document.querySelector(".column2");
+const topExperience = document.querySelector(".topExperience");
 
 
 function appendWorkExperience(column, jobTitle, employer, date, description) {
 
     var workexperience1 = document.createElement("div");
-    workexperience1.classList.add("1work-experience");
+    workexperience1.classList.add("work-experience1");
 
     // Create and append h3 element for job title
     var h3 = document.createElement('h3');
@@ -28,15 +29,16 @@ function appendWorkExperience(column, jobTitle, employer, date, description) {
 }
 
 
-appendWorkExperience(column1, "1st Assistant Director", "Datura Daydream | ","2022", "Short film production.");
-appendWorkExperience(column1,"1st Assistant Director", "The Morning After | ","2022", "Short film production with Roundhouse.");
-appendWorkExperience(column1,"1st Assistant Director", "Rise Like Water | ","2021", "Short film production.");
-appendWorkExperience(column1,"1st Assistant Director", "Fragile | ","2021", "Short film production.");
-appendWorkExperience(column1, "_________________________","","","");
+appendWorkExperience(topExperience, "1st Assistant Director", "Datura Daydream | ","2022", "Short film production.");
+appendWorkExperience(topExperience,"1st Assistant Director", "The Morning After | ","2022", "Short film production with Roundhouse.");
+appendWorkExperience(topExperience,"1st Assistant Director", "Rise Like Water | ","2021", "Short film production.");
+appendWorkExperience(topExperience,"1st Assistant Director", "Fragile | ","2021", "Short film production.");
+//appendWorkExperience(column1, "____________________________________________","","","");
+
 appendWorkExperience(column1,"Production Assistant", "This Person Does Not Exist | ","2022", "Short film production.");
 appendWorkExperience(column1,"Documentary Film Maker", "Il sole in cantina | ","2019", "Production of a short documentary about music and history. Production of a series of interviews.");
 appendWorkExperience(column1,"Film Festival Organiser", "Some Prefer Cake and Genderbender | ","2014 - 2020", "Administrative work to put the festival together, from getting more volunteers to helping choose the films to be screened.");
-appendWorkExperience(column2,"Film Tutor", "Mediateca di San Lazzaro | ","2019", "Tutor younger people in making a short video.");
+appendWorkExperience(column1,"Film Tutor", "Mediateca di San Lazzaro | ","2019", "Tutor younger people in making a short video.");
 appendWorkExperience(column2,"Events Organiser", "Cassero LGBT Center | ","2015 - 2020", "Coordinated a team to organise events and awareness campaigns. Did a lot of team-building.");
 appendWorkExperience(column2,"Internship TV", "Twofour54 | ","2018", "Internship in television where I rehearsed every role in a live news programme.");
 appendWorkExperience(column2,"Documentary Film Maker", "Younet | ","2017 - 2018", "Cultural exchange in Germany to make a documentary about young jugglers. Interviewer.");
