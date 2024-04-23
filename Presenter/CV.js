bf92e1b46a00d8c1f11766992c6a6d297b9219d8
chiagger <busca.chiara.cb@gmail.com>
2024-04-23 14:15:13 +0200

    Moved PVC img to top

--- Presenter/CV.js
+++ Presenter/CV.js
@@ -2,55 +2,151 @@ const column1 = document.querySelector(".column1");
 const column2 = document.querySelector(".column2");
 const topExperience = document.querySelector(".topExperience");
 
-
 function appendWorkExperience(column, jobTitle, employer, date, description) {
+  var workexperience1 = document.createElement("div");
+  workexperience1.classList.add("work-experience1");
 
-    var workexperience1 = document.createElement("div");
-    workexperience1.classList.add("work-experience1");
+  // Create and append h3 element for job title
+  var h3 = document.createElement("h3");
+  h3.textContent = jobTitle;
+  workexperience1.appendChild(h3);
 
-    // Create and append h3 element for job title
-    var h3 = document.createElement('h3');
-    h3.textContent = jobTitle;
-    workexperience1.appendChild(h3);
+  // Create and append h4 element for date range
+  var h4 = document.createElement("h4");
+  h4.textContent = employer + date;
+  workexperience1.appendChild(h4);
 
-    // Create and append h4 element for date range
-    var h4 = document.createElement('h4');
-    h4.textContent = employer + date;
-    workexperience1.appendChild(h4);
+  // Create and append p element for description
+  var p = document.createElement("p");
+  p.textContent = description;
+  workexperience1.appendChild(p);
 
-    // Create and append p element for description
-    var p = document.createElement('p');
-    p.textContent = description;
-    workexperience1.appendChild(p);
-
-    column.appendChild(workexperience1);
-    // Append the new workExperienceDiv to the container div
-   
+  column.appendChild(workexperience1);
+  // Append the new workExperienceDiv to the container div
 }
 
-appendWorkExperience(topExperience, "1st Assistant Director", "That's All From Me | ","2024", "");
-appendWorkExperience(topExperience, "1st Assistant Director", "PVC Real Estate | ","2024", "");
-appendWorkExperience(topExperience, "1st Assistant Director", "The Barn | ","2024", "");
-appendWorkExperience(topExperience, "1st Assistant Director", "In Two Minds | ","2024", "");
-appendWorkExperience(topExperience, "1st Assistant Director", "You Won't Help Me | ","2023 - 2024", "");
-appendWorkExperience(topExperience, "1st Assistant Director", "Datura Daydream | ","2022", "");
-appendWorkExperience(topExperience, "1st Assistant Director", "The Morning After | ","2022", "");
-appendWorkExperience(topExperience, "1st Assistant Director", "Rise Like Water | ","2021", "");
-appendWorkExperience(topExperience, "1st Assistant Director", "Fragile | ","2021", "");
-
+appendWorkExperience(
+  topExperience,
+  "1st Assistant Director",
+  "That's All From Me | ",
+  "2024",
+  ""
+);
+appendWorkExperience(
+  topExperience,
+  "1st Assistant Director",
+  "PVC Real Estate | ",
+  "2024",
+  ""
+);
+appendWorkExperience(
+  topExperience,
+  "1st Assistant Director",
+  "The Barn | ",
+  "2024",
+  ""
+);
+appendWorkExperience(
+  topExperience,
+  "1st Assistant Director",
+  "In Two Minds | ",
+  "2024",
+  ""
+);
+appendWorkExperience(
+  topExperience,
+  "1st Assistant Director",
+  "You Won't Help Me | ",
+  "2023 - 2024",
+  ""
+);
+appendWorkExperience(
+  topExperience,
+  "1st Assistant Director",
+  "Datura Daydream | ",
+  "2022",
+  ""
+);
+appendWorkExperience(
+  topExperience,
+  "1st Assistant Director",
+  "The Morning After | ",
+  "2022",
+  ""
+);
+appendWorkExperience(
+  topExperience,
+  "1st Assistant Director",
+  "Rise Like Water | ",
+  "2021",
+  ""
+);
+appendWorkExperience(
+  topExperience,
+  "1st Assistant Director",
+  "Fragile | ",
+  "2021",
+  ""
+);
 
 //appendWorkExperience(column1, "____________________________________________","","","");
 
-appendWorkExperience(column1,"Production Assistant", "This Person Does Not Exist | ","2022", "Short film production.");
-appendWorkExperience(column1,"Documentary Film Maker", "Il sole in cantina | ","2019", "Production of a short documentary about music and history. Production of a series of interviews.");
-appendWorkExperience(column1,"Film Festival Organiser", "Some Prefer Cake and Genderbender | ","2014 - 2020", "Administrative work to put the festival together, from getting more volunteers to helping choose the films to be screened.");
-appendWorkExperience(column1,"Film Tutor", "Mediateca di San Lazzaro | ","2019", "Tutor younger people in making a short video.");
-appendWorkExperience(column2,"Events Organiser", "Cassero LGBT Center | ","2015 - 2020", "Coordinated a team to organise events and awareness campaigns. Did a lot of team-building.");
-appendWorkExperience(column2,"Internship TV", "Twofour54 | ","2018", "Internship in television where I rehearsed every role in a live news programme.");
-appendWorkExperience(column2,"Documentary Film Maker", "Younet | ","2017 - 2018", "Cultural exchange in Germany to make a documentary about young jugglers. Interviewer.");
-appendWorkExperience(column2,"Director", "Salaborsa, Cineteca di Bologna | ","2014 - 2015", "Production of a short film for the municipal library.");
-
-
+appendWorkExperience(
+  column1,
+  "Production Assistant",
+  "This Person Does Not Exist | ",
+  "2022",
+  "Short film production."
+);
+appendWorkExperience(
+  column1,
+  "Documentary Film Maker",
+  "Il sole in cantina | ",
+  "2019",
+  "Production of a short documentary about music and history. Production of a series of interviews."
+);
+appendWorkExperience(
+  column1,
+  "Film Festival Organiser",
+  "Some Prefer Cake and Genderbender | ",
+  "2014 - 2020",
+  "Administrative work to put the festival together, from getting more volunteers to helping choose the films to be screened."
+);
+appendWorkExperience(
+  column1,
+  "Film Tutor",
+  "Mediateca di San Lazzaro | ",
+  "2019",
+  "Tutor younger people in making a short video."
+);
+appendWorkExperience(
+  column2,
+  "Events Organiser",
+  "Cassero LGBT Center | ",
+  "2015 - 2020",
+  "Coordinated a team to organise events and awareness campaigns. Did a lot of team-building."
+);
+appendWorkExperience(
+  column2,
+  "Internship TV",
+  "Twofour54 | ",
+  "2018",
+  "Internship in television where I rehearsed every role in a live news programme."
+);
+appendWorkExperience(
+  column2,
+  "Documentary Film Maker",
+  "Younet | ",
+  "2017 - 2018",
+  "Cultural exchange in Germany to make a documentary about young jugglers. Interviewer."
+);
+appendWorkExperience(
+  column2,
+  "Director",
+  "Salaborsa, Cineteca di Bologna | ",
+  "2014 - 2015",
+  "Production of a short film for the municipal library."
+);
 
 /*<h2>Work Experience</h2>
 <h3>Event Photographer</h3>
--- Presenter/landingpage.js
+++ Presenter/landingpage.js
@@ -1,167 +1,171 @@
-const titles = ["Datura Daydreams", "Rise Like Water", "Morning After", "Fragile", "PVC Real Estate"]
-const credits = ["directed by Jonah Garrett-Bannister", "directed by Jonah Garrett-Bannister", "directed by Saoirse D. Byrne", "directed by Laura Esteban", "directed by Holly Hunter"]
-const briefPlot = ["Sam is bored of hookup culture, and while trying to find the balance between superficial appearance and genuine attraction he discovers an escape: an ugly thorned flower where he convinces himself the man of his dreams resides.", 
-"Lena comes across a group of nomads in London who help her come to terms with her troubled past by connecting her to spirit and balance. By turning her back on the establishment, she ultimately sees her connection to the natural world and learns the need to fight for it.", 
-"Three queer friends make their way home from a night out. Throughout the film, they will question their own self-censorship, and the place in which it is coming from – is it out of fear, embarrassment, self-preservation, or a combination of the three? And how can we liberate ourselves from this judgment not only from others but from ourselves, and become comfortable in being unapologetically queer?", 
-"A music video for the song Fragile by Noor Mandviwalla.", "A music video for the song PVC Real Estate by Shelf Lives."]
+const titles = [
+  "Datura Daydreams",
+  "Rise Like Water",
+  "Morning After",
+  "Fragile",
+  "PVC Real Estate",
+];
+const credits = [
+  "directed by Jonah Garrett-Bannister",
+  "directed by Jonah Garrett-Bannister",
+  "directed by Saoirse D. Byrne",
+  "directed by Laura Esteban",
+  "directed by Holly Hunter",
+];
+const briefPlot = [
+  "Sam is bored of hookup culture, and while trying to find the balance between superficial appearance and genuine attraction he discovers an escape: an ugly thorned flower where he convinces himself the man of his dreams resides.",
+  "Lena comes across a group of nomads in London who help her come to terms with her troubled past by connecting her to spirit and balance. By turning her back on the establishment, she ultimately sees her connection to the natural world and learns the need to fight for it.",
+  "Three queer friends make their way home from a night out. Throughout the film, they will question their own self-censorship, and the place in which it is coming from – is it out of fear, embarrassment, self-preservation, or a combination of the three? And how can we liberate ourselves from this judgment not only from others but from ourselves, and become comfortable in being unapologetically queer?",
+  "A music video for the song Fragile by Noor Mandviwalla.",
+  "A music video for the song PVC Real Estate by Shelf Lives.",
+];
 
+document.addEventListener("DOMContentLoaded", function () {
+  const dropdownIcon = document.getElementById("dropdown-icon");
+  const sidebar = document.querySelector(".sidebar");
+  const header = document.querySelector("header");
+  const dropdownDiv = document.querySelector(".dropdownicon");
 
-document.addEventListener('DOMContentLoaded', function () {
-    const dropdownIcon = document.getElementById("dropdown-icon");
-    const sidebar = document.querySelector('.sidebar');
-    const header = document.querySelector("header");
-    const dropdownDiv = document.querySelector(".dropdownicon");
+  dropdownIcon.addEventListener("click", () => {
+    sidebar.classList.toggle("active");
+    header.classList.toggle("active");
+    dropdownDiv.classList.toggle("active");
+  });
 
-    dropdownIcon.addEventListener('click', () => {
-        sidebar.classList.toggle('active');
-        header.classList.toggle("active");
-        dropdownDiv.classList.toggle("active");
-    });
+  //sidebar redirects
+  const homeBtn = document.getElementById("home");
+  const CVBtn = document.getElementById("CV");
+  const contactBtn = document.getElementById("contact");
+  const currentPage = window.location.pathname;
 
+  homeBtn.addEventListener("click", () => {
+    if (currentPage !== "/index.html") {
+      window.location.href = "index.html";
+    }
+  });
 
-    //sidebar redirects
-    const homeBtn = document.getElementById("home");
-    const CVBtn = document.getElementById("CV");
-    const contactBtn = document.getElementById("contact");
-    const currentPage = window.location.pathname;
+  CVBtn.addEventListener("click", () => {
+    if (currentPage !== "/CV.html") {
+      window.location.href = "CV.html";
+    }
+  });
 
-    homeBtn.addEventListener("click", () => {
-        if (currentPage !== "/index.html") {
-            window.location.href = "index.html";
-        }
-    });
-
-
-    CVBtn.addEventListener("click", () => {
-        if (currentPage !== "/CV.html") {
-            window.location.href = "CV.html";
-        }
-    });
-
-    contactBtn.addEventListener("click", () => {
-        if (currentPage !== "/contact.html") {
-            window.location.href = "contact.html";
-        }
-    });
+  contactBtn.addEventListener("click", () => {
+    if (currentPage !== "/contact.html") {
+      window.location.href = "contact.html";
+    }
+  });
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+  // Get all grid items
+  const gridItems = document.querySelectorAll(".grid-item");
 
+  // Loop through each grid item
+  gridItems.forEach((item, index) => {
+    const slideshow = item.querySelector(".slideshow");
+    const images = slideshow.querySelectorAll("img");
+    let currentIndex = 0;
 
-
-
-document.addEventListener('DOMContentLoaded', function () {
-    // Get all grid items
-    const gridItems = document.querySelectorAll('.grid-item');
-
-    // Loop through each grid item
-    gridItems.forEach((item, index) => {
-        const slideshow = item.querySelector('.slideshow');
-        const images = slideshow.querySelectorAll('img');
-        let currentIndex = 0;
-
-        // Function to show the current image and hide others
-        function showCurrentImage() {
-            images.forEach((image, i) => {
-                if (i === currentIndex) {
-                    image.style.display = 'block';
-                } else {
-                    image.style.display = 'none';
-                }
-            });
+    // Function to show the current image and hide others
+    function showCurrentImage() {
+      images.forEach((image, i) => {
+        if (i === currentIndex) {
+          image.style.display = "block";
+        } else {
+          image.style.display = "none";
         }
+      });
+    }
 
-        // Function to switch to the next image
-        function nextImage() {
-            currentIndex = (currentIndex + 1) % images.length;
-            showCurrentImage();
-        }
+    // Function to switch to the next image
+    function nextImage() {
+      currentIndex = (currentIndex + 1) % images.length;
+      showCurrentImage();
+    }
 
-        // Function to switch to the previous image
-        function prevImage() {
-            currentIndex = (currentIndex - 1 + images.length) % images.length;
-            showCurrentImage();
-        }
+    // Function to switch to the previous image
+    function prevImage() {
+      currentIndex = (currentIndex - 1 + images.length) % images.length;
+      showCurrentImage();
+    }
 
-        // Set up click event listeners for navigation buttons
-        item.querySelector('.next-btn').addEventListener('click', (event) => {
-            event.stopPropagation(); // Prevent the click from reaching the grid item
-            nextImage();
-        });
-
-        item.querySelector('.prev-btn').addEventListener('click', (event) => {
-            event.stopPropagation(); // Prevent the click from reaching the grid item
-            prevImage();
-        });
-
-        item.addEventListener('click', (event) => {
-            // Check if the click was on the navigation buttons
-            const isNavButtonClick = event.target.classList.contains('next-btn') || event.target.classList.contains('prev-btn');
-            
-            if (!isNavButtonClick) {
-                openPopup(images, currentIndex, index);
-            }
-        });
-
-        // Initial display of images
-        showCurrentImage();
+    // Set up click event listeners for navigation buttons
+    item.querySelector(".next-btn").addEventListener("click", (event) => {
+      event.stopPropagation(); // Prevent the click from reaching the grid item
+      nextImage();
     });
+
+    item.querySelector(".prev-btn").addEventListener("click", (event) => {
+      event.stopPropagation(); // Prevent the click from reaching the grid item
+      prevImage();
+    });
+
+    item.addEventListener("click", (event) => {
+      // Check if the click was on the navigation buttons
+      const isNavButtonClick =
+        event.target.classList.contains("next-btn") ||
+        event.target.classList.contains("prev-btn");
+
+      if (!isNavButtonClick) {
+        openPopup(images, currentIndex, index);
+      }
+    });
+
+    // Initial display of images
+    showCurrentImage();
+  });
 });
-
-
 
 function openPopup(images, currentIndex, gridItemIndex) {
-    const popup = document.getElementById('popup');
-    const imagesPopup = images;
-    const nextBtn = popup.querySelector('.next-btn-popup');
-    const prevBtn = popup.querySelector('.prev-btn-popup');
+  const popup = document.getElementById("popup");
+  const imagesPopup = images;
+  const nextBtn = popup.querySelector(".next-btn-popup");
+  const prevBtn = popup.querySelector(".prev-btn-popup");
 
-    let currentPopupIndex = currentIndex;
+  let currentPopupIndex = currentIndex;
 
+  //let filmNr;
 
-    //let filmNr;
+  function showCurrentPopupImage() {
+    const activePopup = document.querySelector(".active-popup");
+    imagesPopup.forEach((image, index) => {
+      if (index === currentPopupIndex) {
+        activePopup.src = image.src;
+        image.style.display = "block";
+        setImageInfo(gridItemIndex);
+      } else {
+        image.style.display = "none";
+      }
+    });
+  }
 
-    function showCurrentPopupImage() {
-        const activePopup = document.querySelector(".active-popup");
-        imagesPopup.forEach((image, index) => {
-            if (index === currentPopupIndex) {
-                activePopup.src = image.src;
-                image.style.display = 'block';
-                setImageInfo(gridItemIndex);
-            } else {
-                image.style.display = 'none';
-            }
-        });
-    }
+  function setImageInfo(gridItemIndex) {
+    const popupInfo = document.querySelector(".popup-info");
+    popupInfo.querySelector("h2").textContent = titles[gridItemIndex];
+    popupInfo.querySelector("h4").textContent = credits[gridItemIndex];
+    popupInfo.querySelectorAll("p")[0].textContent = briefPlot[gridItemIndex];
+  }
 
-
-    function setImageInfo(gridItemIndex) {
-        const popupInfo = document.querySelector('.popup-info');
-        popupInfo.querySelector('h2').textContent = titles[gridItemIndex];
-        popupInfo.querySelector('h4').textContent = credits[gridItemIndex];
-        popupInfo.querySelectorAll('p')[0].textContent = briefPlot[gridItemIndex];
-    }
-
-
-    function nextPopupImage() {
-        currentPopupIndex = (currentPopupIndex + 1) % images.length;
-        showCurrentPopupImage();
-    }
-
-    function prevPopupImage() {
-        currentPopupIndex = (currentPopupIndex - 1 + images.length) % images.length;
-        showCurrentPopupImage();
-    }
-
-    nextBtn.addEventListener('click', nextPopupImage);
-    prevBtn.addEventListener('click', prevPopupImage);
-
+  function nextPopupImage() {
+    currentPopupIndex = (currentPopupIndex + 1) % images.length;
     showCurrentPopupImage();
+  }
 
-    popup.style.display = 'flex';
+  function prevPopupImage() {
+    currentPopupIndex = (currentPopupIndex - 1 + images.length) % images.length;
+    showCurrentPopupImage();
+  }
+
+  nextBtn.addEventListener("click", nextPopupImage);
+  prevBtn.addEventListener("click", prevPopupImage);
+
+  showCurrentPopupImage();
+
+  popup.style.display = "flex";
 }
 
-
 function closePopup() {
-    const popup = document.getElementById('popup');
-    popup.style.display = 'none';
+  const popup = document.getElementById("popup");
+  popup.style.display = "none";
 }
